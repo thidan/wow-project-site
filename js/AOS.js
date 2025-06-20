bd1d96cb68e1d3f71f31e89d5f231c262e894c1a
Thiago Raman <thiagoraman1@gmail.com>
2025-06-20 09:12:30 -0300

    Animate Header with AOS

---
+++ js/AOS.js
@@ -0,0 +1,11 @@
+AOS.init({
+  // duração da animação (em ms)
+  duration: 1000,
+
+  // atraso antes de iniciar a animação (em ms)
+  delay: 0,
+
+  // timing function (como no CSS) para suavizar a animação
+  easing: 'ease',
+
+});
