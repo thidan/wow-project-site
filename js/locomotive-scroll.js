179915759c274e6f12e617bec94254020d673fbd
Thiago Raman <thiagoraman1@gmail.com>
2025-06-19 21:05:24 -0300

    Make header

---
+++ js/locomotive-scroll.js
@@ -0,0 +1,4 @@
+const scroll = new LocomotiveScroll({
+    el:document.querySelector([data-scroll-containe]),
+    smooth:true,
+});
---
+++ js/tilt.js
@@ -0,0 +1,3 @@
+VanillaTilt.init(document.querySelector(".tilt-box"), {
+    
+});
