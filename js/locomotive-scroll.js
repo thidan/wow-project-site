1dc929df5b153982d9871b84ae790e82d86a078e
Thiago Raman <thiagoraman1@gmail.com>
2025-06-20 18:35:05 -0300

    Make news section

--- js/locomotive-scroll.js
+++ js/locomotive-scroll.js
@@ -2,3 +2,10 @@ const scroll = new LocomotiveScroll({
     el:document.querySelector('[data-scroll-container]'),
     smooth:true,
 });
+
+const noticias = document.querySelector('#noticias');
+        const target = document.querySelector('#target');
+
+        noticias.addEventListener('click', () => {
+            scroll.scrollTo(target);
+        });
